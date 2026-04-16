bdf19379678adb2cb79e23093d6d32e49e26ff79
Sakeenathul Kubra <ksakeenathulkubra@gmail.com>
2026-04-16 10:50:07 +0530

    KDP automation

--- src/tests/login.spec.ts
+++ src/tests/login.spec.ts
@@ -5,8 +5,23 @@ import bookShelf from '../pages/bookshelfPage';
 test('Login to KDP', async ({ page }) => {
   const SignIn = new signIn(page);
   await SignIn.goToKdp();
-  await SignIn.enterEmailId('sakeenathulkubrak@gmail.com');
-  await SignIn.enterPassword('testtest');
+  //await SignIn.enterEmailId('sakeenathulkubrak@gmail.com');
+  //await SignIn.enterPassword('testtest');
+  await SignIn.enterEmailId(process.env.userid!);
+  await SignIn.enterPassword(process.env.password!);
   const BookShelf = new bookShelf(page);
   await BookShelf.verifyBookshelfPageIsLoaded();
 });
+
+test.skip('Login using dotenv', async({page}) =>{
+
+  console.log(process.env.NODE_ENV);
+  console.log(process.env.userid);
+  console.log(process.env.password);
+})
+
+test('Verify the UI of login page', async ({page})=>{
+  const SignIn = new signIn(page);
+  await SignIn.goToKdp();
+  await SignIn.checkTextVisible();
+})
